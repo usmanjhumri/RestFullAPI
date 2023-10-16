472f9d91b986e9182c189d971672aec0168c421d
usman latif <Sahhad@HexaTechSolution.com>
2023-10-16 17:56:41 +0500

    making singup routes

---
+++ routes/singup.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const router = express.Router()
+
+
+
+router.post('/singup', (req, res) => {
+    res.send('signup post method route successfully created')
+})
+
+router.get('/singup', (req, res) => {
+    res.send('signup route successfully created')
+})
+
+
+module.exports = router
