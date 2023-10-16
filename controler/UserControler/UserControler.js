5476c4dc0dc1c6cdd15e857481a7889420d2c1d9
usman latif <Sahhad@HexaTechSolution.com>
2023-10-17 00:04:14 +0500

    updatedd

--- controler/UserControler/UserControler.js
+++ controler/UserControler/UserControler.js
@@ -1,16 +1,24 @@
 const {response} = require('express')
 
-const Users = require('../../model/UserSchema')
+const {Users} = require('../../model/UserSchema.js')
 
 const register = async (req, res) => { 
+    const {
+        username,
+        email,
+        password,
+        phone,
+    } = req.body
+
     try {
-
-        console.log(req.body);
-
-
-      
-        
+        const user = await Users.findeOne({email})
+        console.log(user, ' users');
+        if(user){
+            res.send({ success: false, message: "This email is already exist" });
+        }
     } catch (error) {
         
     }
 }
+
+module.exports = register
--- routes/singup.js
+++ routes/singup.js
@@ -1,11 +1,22 @@
 const express = require('express')
-
+const Users = require('../model/UserSchema')
 const router = express.Router()
 
 
 
-router.post('/singup', (req, res) => {
-    res.send('signup post methods route successfully created')
+router.post('/singup', async (req, res) => {
+    // res.send('signup post methods route successfully created')
+    const {
+        username,
+        email,
+        password,
+        phone,
+    } = req.body
+    try {
+        
+    } catch (error) {
+        
+    }
 })
 
 router.get('/singup', (req, res) => {
